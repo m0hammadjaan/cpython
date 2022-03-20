3cbd816aad34205b79d96b8f2b508f3db53488c0
Muhammad Jan <janmuhammad2050@gmail.com>
2022-03-20 19:54:23 +0500

    Update pymath.c
    **Extra Spaces Removed**

--- Python/pymath.c
+++ Python/pymath.c
@@ -1,6 +1,4 @@
 #include "Python.h"
-
-
 #ifdef HAVE_GCC_ASM_FOR_X87
 // Inline assembly for getting and setting the 387 FPU control word on
 // GCC/x86.
@@ -10,10 +8,7 @@ __attribute__((no_sanitize_memory))
 unsigned short _Py_get_387controlword(void) {
     unsigned short cw;
     __asm__ __volatile__ ("fnstcw %0" : "=m" (cw));
-    return cw;
-}
-
+    return cw;}
 void _Py_set_387controlword(unsigned short cw) {
-    __asm__ __volatile__ ("fldcw %0" : : "m" (cw));
-}
+    __asm__ __volatile__ ("fldcw %0" : : "m" (cw));}
 #endif  // HAVE_GCC_ASM_FOR_X87
